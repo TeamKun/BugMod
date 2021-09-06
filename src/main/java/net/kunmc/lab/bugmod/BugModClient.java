bbfe52e5307195f054d41e348de1c5fa12d31c4b
POne0301 <onexttt@gmail.com>
2021-09-06 21:26:46 +0900

    ログアウト時にリセットするように変更

--- src/main/java/net/kunmc/lab/bugmod/BugModClient.java
+++ src/main/java/net/kunmc/lab/bugmod/BugModClient.java
@@ -4,10 +4,14 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.kunmc.lab.bugmod.block.BlockManager;
 import net.kunmc.lab.bugmod.event.PlayerClientEvent;
+import net.kunmc.lab.bugmod.game.GameManager;
 import net.kunmc.lab.bugmod.networking.ClientNetworking;
 import net.kunmc.lab.bugmod.shader.ShaderManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.RunArgs;
 import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
@@ -18,6 +22,14 @@ public class BugModClient implements ClientModInitializer {
         ClientNetworking.registerReceiver();
         PlayerClientEvent.register();
         ShaderManager.register();
+
+        // 透過のための処理
         BlockRenderLayerMap.INSTANCE.putBlock(BlockManager.bugBlock1, RenderLayer.getCutout());
+        // ログアウト時の処理、他サーバへのログイン時に影響がないようにしておく
+        ClientPlayConnectionEvents.DISCONNECT.register((handler,server)-> {
+            GameManager.resetGame();
+            MinecraftClient.getInstance().getSkinProvider().loadSkin(MinecraftClient.getInstance().player.getGameProfile(), (type, id, texture) -> {
+            }, true);
+        });
     }
 }
