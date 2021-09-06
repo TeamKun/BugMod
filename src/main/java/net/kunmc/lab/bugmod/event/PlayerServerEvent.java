716783cbcfa87ef2ee6f2b2db9084b8ca15c12ae
POne0301 <onexttt@gmail.com>
2021-09-07 01:13:05 +0900

    変更しないブロックを追加

--- src/main/java/net/kunmc/lab/bugmod/event/PlayerServerEvent.java
+++ src/main/java/net/kunmc/lab/bugmod/event/PlayerServerEvent.java
@@ -40,7 +40,7 @@ public class PlayerServerEvent {
 
                         Block b = world.getBlockState(bPos).getBlock();
                         if (b == Blocks.AIR || b instanceof BedBlock || b instanceof SignBlock || b instanceof DoorBlock ||
-                                b == Blocks.LAVA || b == Blocks.WATER || b == Blocks.OBSIDIAN ||
+                                b == Blocks.LAVA || b == Blocks.WATER || b == Blocks.OBSIDIAN || b == Blocks.NETHER_PORTAL ||
                                 b == Blocks.END_GATEWAY || b == Blocks.END_PORTAL || b == Blocks.END_PORTAL_FRAME ||
                                 b == Blocks.COAL_ORE || b == Blocks.IRON_ORE || b == Blocks.DIAMOND_ORE || b == Blocks.SPAWNER || b == Blocks.CHEST)
                             continue;
