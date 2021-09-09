79d59dbaafb9c9b0c98916698df1cfbb14d4d6ce
POne0301 <onexttt@gmail.com>
2021-09-10 03:56:52 +0900

    水/溶岩バケツ仕様時のバグのレベル更新の不具合修正

--- src/main/java/net/kunmc/lab/bugmod/BugModClient.java
+++ src/main/java/net/kunmc/lab/bugmod/BugModClient.java
@@ -6,12 +6,10 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.kunmc.lab.bugmod.block.BlockManager;
-import net.kunmc.lab.bugmod.event.PlayerClientEvent;
 import net.kunmc.lab.bugmod.game.GameManager;
 import net.kunmc.lab.bugmod.networking.ClientNetworking;
 import net.kunmc.lab.bugmod.shader.ShaderManager;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.RunArgs;
 import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
@@ -20,7 +18,6 @@ public class BugModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientNetworking.registerReceiver();
-        PlayerClientEvent.register();
         ShaderManager.register();
 
         // 透過のための処理
---
+++ src/main/java/net/kunmc/lab/bugmod/mixin/BucketItemMixin.java
@@ -0,0 +1,23 @@
+package net.kunmc.lab.bugmod.mixin;
+
+import net.kunmc.lab.bugmod.networking.ClientNetworking;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BucketItem.class)
+public class BucketItemMixin {
+    @Inject(at = @At("HEAD"), method = "onEmptied", cancellable = true)
+    public void onUseItem(World world, ItemStack stack, BlockPos pos, CallbackInfo ci) {
+        if (stack.getItem() == Items.WATER_BUCKET ||
+                stack.getItem() == Items.LAVA_BUCKET){
+            ClientNetworking.sendBreakScreenLevel();
+        }
+    }
+}
